0b7cc739d4a31361ecbf4be99d61a3f7299b72d3
Arc <arky.happ@gmail.com>
2020-05-30 18:10:53 +0300

    Updates

---
+++ precache-manifest.43fe38d74536c97a63f1dda61040cf81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be3be9c43bff253845abddcad8ca2ed5",
+    "url": "/tile-match-game-v2/index.html"
+  },
+  {
+    "revision": "17342eb2b94235719baf",
+    "url": "/tile-match-game-v2/static/css/main.a4335ccd.chunk.css"
+  },
+  {
+    "revision": "d23ecac18f87ead272cd",
+    "url": "/tile-match-game-v2/static/js/2.8f53814d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tile-match-game-v2/static/js/2.8f53814d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17342eb2b94235719baf",
+    "url": "/tile-match-game-v2/static/js/main.be90e90f.chunk.js"
+  },
+  {
+    "revision": "cecd2686145beeffb5f8",
+    "url": "/tile-match-game-v2/static/js/runtime-main.cf7c7a95.js"
+  }
+]);
